1b4a6c83c978fce055a6efa0d139c05609f2fb11
Emil Dziuba <dziubaemil@gmail.com>
2021-07-23 07:19:08 +0200

    Added controller

---
+++ src/main/java/com/emildziuba/bds/beerservice/web/controller/BeerController.java
@@ -0,0 +1,30 @@
+package com.emildziuba.bds.beerservice.web.controller;
+
+import com.emildziuba.bds.beerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeer(@PathVariable("beerId") UUID beerId) {
+        //todo
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<HttpStatus> saveNewBeer(@RequestBody BeerDto beerDto) {
+        //todo
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity updateBeerById(@PathVariable("beerId") UUID beerId, @RequestBody BeerDto beerDto) {
+        //todo
+        return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.NO_CONTENT);
+    }
+}
